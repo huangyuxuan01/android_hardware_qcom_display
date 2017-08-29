2ee1efc5e4d213c82b89febd63ed3c3986b7b2db
huangyuxuan01 <huangyuxuan01@live.com>
2017-08-29 15:51:50 +0800

    msm8974: add CAF-6.0 HAL for Oreo

--- Android.mk
+++ Android.mk
@@ -21,8 +21,12 @@ ifneq ($(TARGET_BOARD_AUTO),true)
             ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
               include $(call all-named-subdir-makefiles,msm8996)
             else
-              ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
-                include $(call all-named-subdir-makefiles,msm8998)
+               ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+                 include $(call all-named-subdir-makefiles,msm8974)
+               else
+                 ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
+                   include $(call all-named-subdir-makefiles,msm8998)
+                 endif
               endif
             endif
           endif
